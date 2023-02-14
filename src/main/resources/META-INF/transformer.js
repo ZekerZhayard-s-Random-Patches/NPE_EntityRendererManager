bc07a0ec0c00daa63f4906a7049aa0105fd45001
ZekerZhayard <trees1000@qq.com>
2023-02-15 00:43:17 +0800

    1.1

--- src/main/resources/META-INF/transformer.js
+++ src/main/resources/META-INF/transformer.js
@@ -1,9 +1,8 @@
 
 var ASMAPI = Java.type("net.minecraftforge.coremod.api.ASMAPI");
 var Opcodes = Java.type("org.objectweb.asm.Opcodes");
-var InsnNode = Java.type("org.objectweb.asm.tree.InsnNode");
 var MethodInsnNode = Java.type("org.objectweb.asm.tree.MethodInsnNode");
-var TypeInsnNode = Java.type("org.objectweb.asm.tree.TypeInsnNode");
+var VarInsnNode = Java.type("org.objectweb.asm.tree.VarInsnNode");
 
 function initializeCoreMod() {
     return {
@@ -11,18 +10,16 @@ function initializeCoreMod() {
             "target": {
                 "type": "METHOD",
                 "class": "net/minecraft/client/renderer/entity/EntityRendererManager",
-                "methodName": "<init>",
-                "methodDesc": "(Lnet/minecraft/client/renderer/texture/TextureManager;Lnet/minecraft/client/renderer/ItemRenderer;Lnet/minecraft/resources/IReloadableResourceManager;Lnet/minecraft/client/gui/FontRenderer;Lnet/minecraft/client/GameSettings;)V"
+                "methodName": "func_225626_a_",
+                "methodDesc": "(Lnet/minecraft/entity/Entity;Lnet/minecraft/client/renderer/culling/ClippingHelper;DDD)Z"
             },
             "transformer": function (mn) {
                 var insnList = mn.instructions.toArray();
                 for (var i = 0; i < insnList.length; i++) {
                     var node = insnList[i];
-                    if (node.getOpcode() === Opcodes.PUTFIELD && node.owner.equals("net/minecraft/client/renderer/entity/EntityRendererManager") && node.name.equals(ASMAPI.mapField("field_78729_o")) && node.desc.equals("Ljava/util/Map;")) {
-                        mn.instructions.insertBefore(node, new InsnNode(Opcodes.POP));
-                        mn.instructions.insertBefore(node, new TypeInsnNode(Opcodes.NEW, "java/util/concurrent/ConcurrentHashMap"));
-                        mn.instructions.insertBefore(node, new InsnNode(Opcodes.DUP));
-                        mn.instructions.insertBefore(node, new MethodInsnNode(Opcodes.INVOKESPECIAL, "java/util/concurrent/ConcurrentHashMap", "<init>", "()V", false));
+                    if (node.getOpcode() === Opcodes.INVOKEVIRTUAL && node.owner.equals("net/minecraft/client/renderer/entity/EntityRendererManager") && node.name.equals(ASMAPI.mapMethod("func_78713_a")) && node.desc.equals("(Lnet/minecraft/entity/Entity;)Lnet/minecraft/client/renderer/entity/EntityRenderer;")) {
+                        mn.instructions.insert(node, new MethodInsnNode(Opcodes.INVOKESTATIC, "io/github/zekerzhayard/npe_entityrenderermanager/NPE_EntityRendererManager", "check", "(Lnet/minecraft/client/renderer/entity/EntityRenderer;Lnet/minecraft/entity/Entity;)Lnet/minecraft/client/renderer/entity/EntityRenderer;", false));
+                        mn.instructions.insert(node, new VarInsnNode(Opcodes.ALOAD, 1));
                     }
                 }
                 return mn;
